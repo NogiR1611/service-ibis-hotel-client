243871262d92beed25e45767e82e7307dc617008
NogiR1611 <nogir16111998@gmail.com>
2020-07-25 17:35:42 +0700

    update titipan barang

--- src/Components/inputBarang.js
+++ src/Components/inputBarang.js
@@ -1,20 +1,13 @@
-import React,{Component} from "react";
+import React from "react";
 import "./style.css";
 
-class InputBarang extends Component{
-    render(){
-        return(
-            <div className="request-list">
-                <h1>List titipan barang</h1>
-                <form action="list_barang.html" method="POST">
-                    <label>{this.props.labelTitipanBarang}</label><br />
-                    <input type="number" name={this.props.jumlah_barang} onChange={this.props.handleChange} />
-                    <button type="button" onClick={this.props.list_jumlah}>Oke</button>
-                </form>
-                <div id="test" />
-            </div>
-        );
-    }
-}
+const InputBarang = props => (
+    <input
+        type="number"
+        disabled={props.disabled}
+        name={props.jumlah_barang}
+        onChange={props.handleChange}
+    />
+)
 
 export default InputBarang;
--- src/Titipan_barang.js
+++ src/Titipan_barang.js
@@ -5,17 +5,52 @@ import Footer from "./Components/Footer";
 import "./Components/style.css";
 
 class titipanBarang extends Component{
-    render(){
-        const movePage=function(){
-            window.location.href="/list-barang";
+    constructor(props){
+        super(props)
+        this.state = {
+            jumlah_barang : 0,
+            enabled : false
+
         }
+        this.list_jumlah=this.list_jumlah.bind(this);
+    }
+
+    handleChange=(e)=>{
+        let barang = e.target.value;
+        this.setState({jumlah_barang:barang});
+    }
+
+    list_jumlah(){
+        let barang = this.state.jumlah_barang;
+        let list_input = document.getElementById("test");
+
+        for (let i = 0; i<barang; i++){
+            let elemen = document.createElement("input");
+            let spasi = document.createElement("br");
+            elemen.type = "text";
+            list_input.appendChild(elemen);
+            list_input.appendChild(spasi);
+        }
+        this.setState({enabled:!this.state.enabled});
+    }
+
+    render(){
         return(
             <div>
                 <Header />
-                <InputBarang 
-                    labelTitipanBarang="Silahkan masukan jumlah barang anda"
-                    list_jumlah={movePage}    
-                />
+                <div className="request-list">
+                    <h1>List titipan barang</h1>
+                    <form action="list_barang.html" method="POST">
+                        <label>Silahkan masukan jumlah barang</label><br />
+                        <InputBarang
+                            disabled={this.state.enabled}
+                            handleChange={this.handleChange}
+                            jumlah_barang={this.state.jumlah_barang}
+                        />
+                        <button type="button" onClick={this.list_jumlah}>Oke</button>
+                    </form>
+                    <div id="test" />
+                </div>
                 <Footer />
             </div>
         );
